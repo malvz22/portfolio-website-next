5ad837e955df6ff7d6f173eed7ed12e4c8708c41
Malvin A <malvinaudriansyah77@gmail.com>
2025-07-18 17:36:42 +0700

    code formatting on skills & tech

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,55 +94,39 @@ export default function Home() {
           </div>
           <div className="flex flex-col gap-3 max-w-full">
             <Heading title="Skills & Tech" centered={false} />
-            {/* <div className="flex flex-col md:flex-row gap-4 items-center w-full max-w-full">
-              <div className="w-full max-w-full text-start text-sm md:text-base lg:text-lg ">
-                <p className="mb-3">
-                  I'm a motivated front-end developer with a passion for
-                  crafting visually engaging and user-friendly websites. While
-                  I'm early in my web development journey, I've been actively
-                  building my skills through self-learning and personal
-                  projects. Through building personal projects, I've leaned to
-                  translate designs into modular, maintainable code.
-                </p>
-                <p className="mb-3">
-                  My core technical skills include HTML, CSS, JavaScript, and
-                  TypeScript. I'm particularly interested in responsive web
-                  designs and ensuring seamless user experience across devices.
-                  I've also explored popular tools and frameworks such as
-                  React.js, Next.js, Tailwind CSS, Bootstrap, and Chakra UI.
-                </p>
-                <p>
-                  I'm now seeking opportunities where I can contribute fresh
-                  ideas, collaborate with like-minded developers, and continue
-                  growing in a forward-thinking environment. I'm excited to
-                  bring my creativity, discipline, and enthusiasm for technology
-                  to a real-world team.
-                </p>
+            <div className="flex flex-col md:flex-row gap-9 items-center">
+              <p className="font-bold text-[20px] flex flex-row gap-6">
+                Tech Stack <span className="hidden md:flex">|</span>
+              </p>
+              <div className="flex flex-row flex-wrap gap-3 justify-center">
+                <Image
+                  src={"/html-5.png"}
+                  alt="html-5"
+                  width={50}
+                  height={50}
+                />
+                <Image src={"/css-3.png"} alt="css-3" width={50} height={50} />
+                <Image src={"/js.png"} alt="js" width={50} height={50} />
+                <Image
+                  src={"/ts-logo-512.png"}
+                  alt="ts"
+                  width={50}
+                  height={50}
+                />
+                <Image src={"/react.png"} alt="react" width={50} height={50} />
+                <Image
+                  src={"/nextjs-icon-light-background.png"}
+                  alt="nextjs"
+                  width={50}
+                  height={50}
+                />
+                <Image
+                  src={"/tailwind.svg"}
+                  alt="tailwind"
+                  width={50}
+                  height={50}
+                />
               </div>
-            </div> */}
-          </div>
-          <div className="flex flex-col md:flex-row gap-9 items-center">
-            <p className="font-bold text-[20px] flex flex-row gap-6">
-              Tech Stack <span className="hidden md:flex">|</span>
-            </p>
-            <div className="flex flex-row flex-wrap gap-3 justify-center">
-              <Image src={"/html-5.png"} alt="html-5" width={50} height={50} />
-              <Image src={"/css-3.png"} alt="css-3" width={50} height={50} />
-              <Image src={"/js.png"} alt="js" width={50} height={50} />
-              <Image src={"/ts-logo-512.png"} alt="ts" width={50} height={50} />
-              <Image src={"/react.png"} alt="react" width={50} height={50} />
-              <Image
-                src={"/nextjs-icon-light-background.png"}
-                alt="nextjs"
-                width={50}
-                height={50}
-              />
-              <Image
-                src={"/tailwind.svg"}
-                alt="tailwind"
-                width={50}
-                height={50}
-              />
             </div>
           </div>
         </section>
