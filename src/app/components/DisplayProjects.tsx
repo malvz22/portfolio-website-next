90eb5739de0742c44c092d31427a8c3167a733b5
Malvin A <malvinaudriansyah77@gmail.com>
2025-07-30 13:58:00 +0700

    feat(project-gallery-slider): all of listed project have screenshots added

--- src/app/components/DisplayProjects.tsx
+++ src/app/components/DisplayProjects.tsx
@@ -36,6 +36,14 @@ const Projects = [
       "This company website is a personal training project built using Next.js, JavaScript, and Tailwind CSS. It showcases a clean and modern design with a focus on user experience, and responsive layout. This project features Home page, About page, Services page, and Teams page. In this project, I learned how to build responsive layouts with Tailwind CSS, implement dynamic routing using Next.js, and fetch external data using APIs. For demonstration purposes, the Team page uses random user data fetched from the Randomuser.me API.",
     repositoryLink: "https://github.com/malvz22/RoguePixel-Company-Website",
     websiteLink: "https://roguepixel-woad.vercel.app/",
+    screenshots: [
+      "/project-screenshots/roguepixel/screenshot-1.webp",
+      "/project-screenshots/roguepixel/screenshot-2.webp",
+      "/project-screenshots/roguepixel/screenshot-3.webp",
+      "/project-screenshots/roguepixel/screenshot-4.webp",
+      "/project-screenshots/roguepixel/screenshot-5.webp",
+      "/project-screenshots/roguepixel/screenshot-6.webp",
+    ],
   },
   {
     name: "Simpang Raya",
@@ -47,6 +55,17 @@ const Projects = [
       "Simpang Raya is a restaurant website built using Next.js, TypeScript, and Tailwind CSS. It was developed to promote Simpang Raya restaurant by showcasing its available menus and multiple restaurant locations. The site features a fully responsive layout and a modern design. This project includes a Home page, About page, Menu page, Locations page, and Contact form. From this project, I learned how to use libraries such as React Slick to implement image sliders.",
     repositoryLink: "https://github.com/malvz22/simpang-raya",
     websiteLink: "https://simpang-raya.vercel.app/",
+    screenshots: [
+      "/project-screenshots/simpang-raya/screenshot-1.webp",
+      "/project-screenshots/simpang-raya/screenshot-2.webp",
+      "/project-screenshots/simpang-raya/screenshot-3.webp",
+      "/project-screenshots/simpang-raya/screenshot-4.webp",
+      "/project-screenshots/simpang-raya/screenshot-1.webp",
+      "/project-screenshots/simpang-raya/screenshot-5.webp",
+      "/project-screenshots/simpang-raya/screenshot-6.webp",
+      "/project-screenshots/simpang-raya/screenshot-7.webp",
+      "/project-screenshots/simpang-raya/screenshot-8.webp",
+    ],
   },
   // {
   //   name: "NextGen Gym",
@@ -69,6 +88,17 @@ const Projects = [
       "OtakuVault is a personal project built using Next.js, TypeScript, and Tailwind CSS that allows users to search for anime and manga titles and view detailed information such as trailers, release dates, synopsis, and more. The website features a responsive and modern layout with a homepage showcasing current season anime, most popular anime, and most popular manga. It also includes a search function for anime and manga, genre-specific list pages, and top anime rankings. From this project, I learned how to fetch and display API data dynamically, build reusable components, and implement client-side filtering and pagination for improved user navigation.",
     repositoryLink: "https://github.com/malvz22/otaku-vault",
     websiteLink: "https://otaku-vault.vercel.app/",
+    screenshots: [
+      "/project-screenshots/otaku-vault/screenshot-1.webp",
+      "/project-screenshots/otaku-vault/screenshot-2.webp",
+      "/project-screenshots/otaku-vault/screenshot-3.webp",
+      "/project-screenshots/otaku-vault/screenshot-4.webp",
+      "/project-screenshots/otaku-vault/screenshot-1.webp",
+      "/project-screenshots/otaku-vault/screenshot-5.webp",
+      "/project-screenshots/otaku-vault/screenshot-6.webp",
+      "/project-screenshots/otaku-vault/screenshot-7.webp",
+      "/project-screenshots/otaku-vault/screenshot-8.webp",
+    ],
   },
   {
     name: "Spoon & Fork",
@@ -80,6 +110,18 @@ const Projects = [
       "Spoon & Fork is a personal project website built using Next.js, TypeScript, and Tailwind CSS. It allows users to discover recipes and explore detailed cooking instructions through a clean, modern, and fully responsive layout. The website features a homepage, recipe search, recipe categories, an About section, and a contact form. From this project, I learned how to manage dynamic routing in Next.js, implement search functionality, and build a responsive UI that enhances user experience.",
     repositoryLink: "https://github.com/malvz22/spoon-and-fork",
     websiteLink: "https://spoon-and-fork-ebon.vercel.app",
+    screenshots: [
+      "/project-screenshots/spoon-and-fork/screenshot-1.webp",
+      "/project-screenshots/spoon-and-fork/screenshot-2.webp",
+      "/project-screenshots/spoon-and-fork/screenshot-3.webp",
+      "/project-screenshots/spoon-and-fork/screenshot-4.webp",
+      "/project-screenshots/spoon-and-fork/screenshot-5.webp",
+      "/project-screenshots/spoon-and-fork/screenshot-6.webp",
+      "/project-screenshots/spoon-and-fork/screenshot-7.webp",
+      "/project-screenshots/spoon-and-fork/screenshot-8.webp",
+      "/project-screenshots/spoon-and-fork/screenshot-9.webp",
+      "/project-screenshots/spoon-and-fork/screenshot-10.webp",
+    ],
   },
   {
     name: "Tenplus",
@@ -91,6 +133,15 @@ const Projects = [
       "This product landing page was a personal creative challenge project developed for Tenplus, an hydration and electrolyte brand. Built using React.js, JavaScript, and Tailwind CSS, it features a clean and modern design focused on product showcase and user engagement. The page is fully responsive and optimized for smooth navigation across devices. It includes key sections such as Hero, About, Product, and Testimonials.",
     repositoryLink: "https://github.com/malvz22/tenplus-landing-page",
     websiteLink: "https://tenplus-seven.vercel.app/",
+    screenshots: [
+      "/project-screenshots/tenplus/screenshot-1.webp",
+      "/project-screenshots/tenplus/screenshot-2.webp",
+      "/project-screenshots/tenplus/screenshot-3.webp",
+      "/project-screenshots/tenplus/screenshot-4.webp",
+      "/project-screenshots/tenplus/screenshot-5.webp",
+      "/project-screenshots/tenplus/screenshot-6.webp",
+      "/project-screenshots/tenplus/screenshot-7.webp",
+    ],
   },
 ];
 
@@ -199,17 +250,19 @@ const DisplayProjects = () => {
 
             <div className="w-full max-w-full mx-auto mb-4">
               <Slider {...settings}>
-                <div className="px-2">
-                  <div className="w-full h-[180px] aspect-auto relative">
-                    <Image
-                      src={selectedProject.imgSrc}
-                      alt={selectedProject.imgAlt}
-                      fill
-                      style={{ objectFit: "cover" }}
-                      sizes="100%"
-                    />
+                {selectedProject.screenshots.map((screenshot, index) => (
+                  <div className="" key={index}>
+                    <div className="w-full h-[180px] aspect-auto relative">
+                      <Image
+                        src={screenshot}
+                        alt={`Screenshot ${index + 1}`}
+                        fill
+                        style={{ objectFit: "contain" }}
+                        sizes="100%"
+                      />
+                    </div>
                   </div>
-                </div>
+                ))}
               </Slider>
             </div>
 
