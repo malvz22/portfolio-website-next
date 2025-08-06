b84ecdb681e94295cd506aa7d5b814bb3a7a16b5
Malvin A <malvinaudriansyah77@gmail.com>
2025-08-06 13:00:31 +0700

    feat(project-modal): added fade in animation for modal

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -11,7 +11,7 @@ const Modal: React.FC<ModalProps> = ({ modalOpen, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/50 text-black">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-[95vw] md:max-w-[90vw] lg:max-w-[900px] max-h-[90vh] md:max-h-[85vh] w-full relative">
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-[95vw] md:max-w-[90vw] lg:max-w-[900px] max-h-[90vh] md:max-h-[85vh] w-full relative animate-fadeIn">
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-600 hover:text-gray-900 transition-colors duration-300"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        slideInBottom: {
+          "0%": { transform: "translateY(100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.45s ease-in-out",
+        slideInBottom: "slideInBottom fadeIn 0.45s ease-in-out",
+      },
       colors: {
         primary: "#E23A2F",
         hover: "#C53030",
